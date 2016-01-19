47fed7efde4c7594371d5c6de14c30caf81b8aa6
Thomas Schwappach <Thomas.schwappach@gmail.com>
2016-01-19 15:00:17 -0700

    changes

---
+++ Inheritance Questions/src/Bird.java
@@ -0,0 +1,31 @@
+
+public class Bird
+	{
+		public runBirdMethod1()
+			{
+				/*some code here*/
+			}
+		public runBirdMethod2()
+			{
+				/*some code here*/
+			}
+		public sound()
+			{
+				return "screach";
+			}
+	}
+public class Wren extends Bird
+	{
+		public runWrenMethod1()
+			{
+				/*some code here*/
+			}
+		public runWrenMethod2()
+			{
+				/*some code here*/
+			}
+		public sound()
+			{
+				();
+			}
+	}
